efa52275a474ae61a6026b4260fab304fb3c7e8a
Cat4848 <catalinrol@gmail.com>
2024-07-26 14:56:12 +0100

    feat: added intellisense for methods signatures

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,15 @@ interface Fail {
 }
 
 export class ResultGenerator {
+  /**
+   * Generate success function that takes a string
+   * and returns a Success object with success and data properties.
+   * The success property is a boolean value.
+   * The data property contains the string passed as parameter.
+   *
+   * @param {string}     data
+   *
+   */
   generateSuccess(data: string) {
     const success: Success = {
       success: true,
@@ -17,6 +26,18 @@ export class ResultGenerator {
     return success;
   }
 
+  /**
+   * The generateError function that takes an "err" unknown parameter
+   * and returns a Fail object with success and error properties.
+   * The success property is a boolean value.
+   * The error property contains an error with "Unknown Error" message
+   * if the "err" parameter is of type unknown.
+   * If the function is passed an Error object, the error property
+   * will contain the error.
+   *
+   * @param {unknown}     err
+   *
+   */
   generateError(err: unknown) {
     let fail: Fail = {
       success: false,
